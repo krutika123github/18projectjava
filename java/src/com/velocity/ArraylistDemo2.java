e6bbcc5ef5e92fd688ae79cf48c382ab32521e87
krutika123456 <lambhadekrutika@gmail.com>
2024-02-01 17:47:26 +0530

    added print line in arraylistdemo2 file

--- java/src/com/velocity/ArraylistDemo2.java
+++ java/src/com/velocity/ArraylistDemo2.java
@@ -1,5 +1,19 @@
 package com.velocity;
 
-public class ArraylistDemo2 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ArraylistDemo2 {
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		
+		System.out.println(list);
+	}
 }
+
+
