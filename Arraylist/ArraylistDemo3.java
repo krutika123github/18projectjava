0444d21e5be3c842cab069c8765c861e46931d39
krutika <lambhadekrutika@gmail.com>
2024-02-01 19:03:02 +0530

    added arraylist code for number and city

---
+++ Arraylist/ArraylistDemo3.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArraylistDemo3 {
+	public static void main(String[] args) {
+		 ArrayList<E> al = new ArrayList();
+		 al.add(10);
+		 al.add(20);
+		 al.add("pune");
+		 al.add("mumbai");
+		 
+		 System.out.println(al);
+	}
+
+}
