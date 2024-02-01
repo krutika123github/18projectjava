005be02d79fd704b20b337698b69f6f5262f582c
krutika123456 <lambhadekrutika@gmail.com>
2024-02-01 13:08:53 +0530

    write meanigful message

---
+++ Arraylist/src/com/velocity/Arraylistdemo.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylistdemo {
+	
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(10);
+		list.add(30);
+		list.add(20);
+		list.add(50);
+		System.out.println(list);
+	}
+
+	}
+
+
